5a7496d9ea86bf0aefc6021dcba651f16c3dd717
Yassine BIZNAOUNE <yassine.biznaoune@e-polytechnique.com>
2023-12-31 22:57:16 +0100

    Add Saudi Roshn League Scoreboard

--- src/app/laliga/page.tsx
+++ src/app/laliga/page.tsx
@@ -1,59 +1,79 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import "./styles/LaLiga.scss";
 import Image from "next/image";
 
 import logo from "./assets/laliga_logo.png";
-import home_team from "./assets/home_team_logo.png"
-import away_team from "./assets/away_team_logo.png"
-
+import home_team from "./assets/home_team_logo.png";
+import away_team from "./assets/away_team_logo.png";
 
 const LaLiga = () => {
+  const [isText, setIsText] = useState(true);
   const date = new Date();
   // const time = date.getHours() + ":" + date.getMinutes();
 
+  const alternateText = () => {
+    setTimeout(() => {
+      setIsText(!isText);
+    }, 4000);
+  };
+
+  alternateText();
   return (
-    <div className="Scoreboard LaLiga">
-      <div className="left_part">
-        <div className="logo">
-          <Image src={logo} alt="" />
-        </div>
-        <div className="left_time_part">
-          <div className="time">
-            <h1 className="time_text">{"45:03"}</h1>
+    <>
+      {/* <h1 className="font-bold text-center py-6 text-red-600">
+        {isText.toString()}
+      </h1> */}
+      <div className="Scoreboard LaLiga">
+        <div className="left_part">
+          <div className="logo">
+            <Image src={logo} alt="" />
           </div>
-          {/* Temporary  */}
-          <div className="add_time">+5</div>
+          <div className="left_time_part">
+            <div className="time">
+              <h1 className="time_text">{"45:03"}</h1>
+            </div>
+            {/* Temporary  */}
+            <div className="add_time">+5</div>
+          </div>
+        </div>
+        <div className="teams">
+          <div className="home team">
+            {isText ? (
+              <span className="team_name">RSO</span>
+            ) : (
+              <Image src={home_team} alt="" />
+            )}
+          </div>
+          <div className="away team">
+            {isText ? (
+              <span className="team_name">BET</span>
+            ) : (
+              <Image src={away_team} alt="" />
+            )}
+          </div>
+        </div>
+        <div className="teams_colors">
+          <div className="home team_color">
+            <div className="color color1"></div>
+            <div className="color color2"></div>
+          </div>
+          <div className="away team_color">
+            <div className="color color1"></div>
+            <div className="color color2"></div>
+          </div>
+        </div>
+        <div className="scores">
+          <div className="home team_score">
+            <h1 className="time_text">0</h1>
+          </div>
+          <div className="away team_score">
+            <h1 className="time_text">1</h1>
+          </div>
         </div>
       </div>
-      <div className="teams">
-        <div className="home team">
-          <Image src = {home_team} alt="" />
-        </div>
-        <div className="away team">
-          <Image src = {away_team} alt="" />
-        </div>
-      </div>
-      <div className="teams_colors">
-        <div className="home team_color">
-          <div className="color color1"></div>
-          <div className="color color2"></div>
-        </div>
-        <div className="away team_color">
-          <div className="color color1"></div>
-          <div className="color color2"></div>
-        </div>
-      </div>
-      <div className="scores">
-        <div className="home team_score">
-          <h1 className="time_text">0</h1>
-        </div>
-        <div className="away team_score">
-          <h1 className="time_text">1</h1>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/app/roshn/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useState } from "react";
+import "./styles/Roshn.scss";
+
+import logo from "./assets/Roshn_Saudi_League_Logo.svg.png";
+import home_team_logo from "./assets/al-nassr.png";
+import away_team_logo from "./assets/al-ittihad.png";
+import bg from "./assets/bg.png";
+import Image from "next/image";
+
+const Roshn = () => {
+  const [isArabic, setIsArabic] = useState(false);
+
+  const alternateTeamName = () => {
+    setTimeout(() => {
+      setIsArabic(!isArabic);
+    }, 4000);
+  };
+
+  alternateTeamName();
+
+  return (
+    <>
+      <div className="bg">
+        <Image src={bg} alt="bg"></Image>
+      </div>
+      <div className="Scoreboard">
+        <div className="logo_section">
+          <Image src={logo} alt="Roshn_Logo"></Image>
+        </div>
+        <div className="score_section">
+          <div className="teams_logos">
+            <div className="away_team team">
+              <Image src={away_team_logo} alt="Roshn_Logo"></Image>
+            </div>
+            <div className="home_team team">
+              <Image src={home_team_logo} alt="Roshn_Logo"></Image>
+            </div>
+          </div>
+          <div className="teams_names">
+            <div className="away_team team">
+              {isArabic ? (
+                <span className="team_name_arabic">الإتحاد</span>
+              ) : (
+                <span className="team_name">ITT</span>
+              )}
+            </div>
+            <div className="home_team team">
+              {isArabic ? (
+                <span className="team_name_arabic">النصر</span>
+              ) : (
+                <span className="team_name">NAS</span>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="time_section_and_score_values">
+          <div className="teams_scores_values">
+            <div className="away_team team">
+              <span className="away_team_score_value">1</span>
+            </div>
+            <div className="home_team team">
+              <span className="home_team_score_value">2</span>
+            </div>
+          </div>
+          <div className="time_section">
+            <span className="regular_time_value">
+              52<span className="two_dots">:</span>69
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Roshn;
